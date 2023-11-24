7751d9196e51062fb4deab3c22fc20867ba2cfc8
Thiago Sabara <thi.sil.sab@gmail.com>
2023-11-23 23:43:29 -0300

    feat: Readme + BAse

--- 04_pilha/bases.py
+++ 04_pilha/bases.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 import numpy as np
+
 
 class Stacks:
     def __init__(self, size: int) -> None:
@@ -6,16 +8,27 @@ class Stacks:
 
         :param size:
         """
-        self.size: int = size
-        self.top_pos: int = -1
-        self.values: np.array = np.empty(self.size, dtype=int)
+        self.__size: int = size
+        self.__top_pos: int = -1
+        self.__values: np.array = np.empty(self.__size, dtype=float)
 
     def __is_full(self) -> bool:
         """Avalia se a pilha esta cheia
 
         Esta função não deve estar acessível para classes externas
         """
-        if self.top_pos == self.size -1:
+        if self.__top_pos == self.__size -1:
             return True
         return False
 
+    def __getitem__(self, item):
+        raise IndexError("Elements cannot be accessed")
+
+    def stack_up(self):...
+
+    def unstack(self):...
+
+if __name__ == "__main__":
+    pilha = Stacks(10)
+
+    pilha[2]
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# datastruct
+# Datastruct
 
 Neste repo estou revisando alguns conteúdos de estruturas de dados e alguns algoritimos que considero importantes.
 
+ - Big O Notation
+ - Filas
+ - Arvores
+ - etc
 
