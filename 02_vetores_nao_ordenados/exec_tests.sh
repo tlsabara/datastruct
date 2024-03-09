d070ea0a7ba319a7f6f4bba7484b25e33402a3e4
Sabará <thi.sil.sab@gmail.com>
2024-03-09 19:25:38 -0300

    Update class name in tests and Basics notebook
    Class name UnordenetVector has been corrected to UnordenedVector in test cases and Basics notebook. The commit also includes the removal of an unused test execution shell script, enhancing the cleanliness of the project structure.

--- 02_vetores_nao_ordenados/exec_tests.sh
+++
@@ -1,1 +0,0 @@
- pytest /home/tluca/git_unix/datastruct/02_vetores_nao_ordenados/tests.py --no-header --no-summary -vv
--- 02_vetores_nao_ordenados/tests.py
+++ 02_vetores_nao_ordenados/tests.py
@@ -1,13 +1,13 @@
 import pytest
-from bases_vectors import UnordenetVector
+from bases_vectors import UnordenedVector
 
 def test_show_values_shold_return_a_string_always() -> None:
     """Testa se o vetor esta restornando uma string
     """
     vector_size = 5
 
-    instace_empty = UnordenetVector(vector_size)
-    instace_valid = UnordenetVector(vector_size)
+    instace_empty = UnordenedVector(vector_size)
+    instace_valid = UnordenedVector(vector_size)
 
     instace_valid.values[0] = 11
     instace_valid.last_pos = vector_size - 1
@@ -19,7 +19,7 @@ def test_show_values_shold_return_a_string_always() -> None:
 def test_insert_a_clear_number_should_not_return_error() -> None:
     """O Vetor deve permitir ao usuário inserir um numero qualquer
     """
-    instace = UnordenetVector(3)
+    instace = UnordenedVector(3)
     instace.insert(1)
     assert len(instace) == 1
 
@@ -30,7 +30,7 @@ def test_insert_a_clear_number_should_not_return_error() -> None:
 def test_insert_should_raise_error_on_try_insert_a_not_int() -> None:
     """A classe não deve permitir ser inserido um valor que não seja um inteiro
     """
-    instace = UnordenetVector(3)
+    instace = UnordenedVector(3)
     with pytest.raises(ValueError) as e:
         instace.insert("Carro")
 
@@ -38,7 +38,7 @@ def test_insert_should_raise_error_on_try_insert_a_not_int() -> None:
 def test_insert_should_raise_error_on_try_insert_more_than_len_of_size() -> None:
     """A classe não deve permitir inserir numeros além do tamanho do vetor
     """
-    instace = UnordenetVector(3)
+    instace = UnordenedVector(3)
 
     instace.insert(1)
     instace.insert(2)
@@ -50,7 +50,7 @@ def test_insert_should_raise_error_on_try_insert_more_than_len_of_size() -> None
 def test_vector_shold_be_indexable() -> None:
     """A classe deve permitir o apontamento do index(como uma lista em python
     """
-    instace = UnordenetVector(3)
+    instace = UnordenedVector(3)
 
     instace.insert(10)
     instace.insert(20)
@@ -64,7 +64,7 @@ def test_vector_shold_be_sliceble() -> None:
     """A classe deve comportar-se como uma lista em um slice
     """
 
-    instace = UnordenetVector(3)
+    instace = UnordenedVector(3)
 
     instace.insert(10)
     instace.insert(20)
@@ -77,7 +77,7 @@ def test_vector_shold_be_sliceble() -> None:
 def test_vector_shold_be_exclude_a_value() -> None:
     """A classe deve permitir a remoção de um item, sem deixar posições vazlias"""
 
-    instace = UnordenetVector(7)
+    instace = UnordenedVector(7)
 
     instace.insert(10)
     instace.insert(20)
